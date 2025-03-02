b06e92719691686a2d2c5f626a33d6afa8d2e5ee
Shandie Brillantes <brillantes.shandie@gmail.com>
2025-03-03 03:45:33 +0800

    Adding Trait

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/users', 'UserController@getUsers');
-
+// API Routes (Make sure to put them outside the first function!)
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/users', ['uses' => 'UserController@getUsers']);
+});
